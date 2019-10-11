fad457eea4c0a1e1d4951f40fbfed9fc858275bb
Fachi <https://github.com/Fachi3>
2019-10-11 00:59:29 -0500

    Split -Split schema and resolvers in ./lib

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
 'use strict'
 
-const {graphql, buildSchema} = require('graphql')
+const { buildSchema } = require('graphql')
 const express = require('express')
 const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3000
 
-//Definir esquema
-const schema = buildSchema(`
-    type Query {
-        hello: String,
-    }
-`)
+// Definir esquema
+const schema = buildSchema(
+  readFileSync(
+    join(__dirname, 'lib', 'schema.graphql'), 'utf-8'
+  )
+)
 
-//Configurar resolvers
-const resolvers = {
-    hello: () => {
-        return 'Hello worl'
-    }
-}
-
-//Set Middleware
+// Set Middleware
 app.use('/api', gqlMiddleware({
-    schema: schema,
-    rootValue: resolvers,
-    graphiql: true
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
 }))
 
-
-//Levantar server
+// Levantar server
 app.listen(port, () => {
-    console.log(`Server is listening at http://localhost:${port}/api`)
+  console.log(`Server is listening at http://localhost:${port}/api`)
 })
